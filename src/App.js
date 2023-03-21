8765736c782671702434d80ce680ccf5050490d6
Rory Donald <rory.alexander.donald@gmail.com>
2023-03-21 17:54:14 -0600

    Initial commit for sentiment analysis app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import positiveGIF from './images/positiveGIF.gif';
+import negativeGIF from './images/negativeGIF.gif';
+import neutralGIF from './images/neutralGIF.gif';
+
+import Sentiment from 'sentiment';
+const sentiment = new Sentiment();
 
 function App() {
+
+  const [phrase, setPhrase] = useState('');
+  const [sentimentScore, setSentimentScore] = useState(null);
+
+  useEffect(() => {
+    setSentimentScore(sentiment.analyze(phrase));
+  }, [phrase]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Sentiment Analysis</h2>
+
+        <input value={phrase} onChange={e => setPhrase(e.target.value)}
+          style={{ padding: '20px', fontSize: '20px', width: '90%' }}
+        />
+
+        {
+          sentimentScore !== null ?
+            <p>Sentiment Score: {sentimentScore.score}</p>
+            : ''
+        }
+
+        {
+          sentimentScore ?
+            sentimentScore.score === 0 ?
+              <img src={neutralGIF} alt="neutral" />
+              :
+              sentimentScore.score > 0 ?
+                <img src={positiveGIF} alt="postive" />
+                :
+                <img src={negativeGIF} alt="negative" />
+            : ''
+        }
+
       </header>
     </div>
   );
